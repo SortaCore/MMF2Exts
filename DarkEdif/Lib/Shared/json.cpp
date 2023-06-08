c756991bd3beb0ad5a77456c18b5a0e1ee77ee47
Phi <phi@dark-wire.com>
2023-06-08 12:02:15 +0100

    DarkEdif: JSON parser: Allow starting with comment
    Addition to 43785f71de4e4d578f6492f719340d9059b5f0a7 to allow JSONs to start with a C-style comment.

--- DarkEdif/Lib/Shared/json.cpp
+++ DarkEdif/Lib/Shared/json.cpp
@@ -865,8 +865,8 @@ int json_clean_comments (const json_char ** json_input, json_state * state, char
 	int comment = 0;
 	const char * json = *json_input, * i;
 
-	// Doesn't start with brace or space: is it a BOM?
-	if (json[0] != '{' && !std::isspace(json[0]))
+	// Doesn't start with brace, comment or space: is it a BOM?
+	if (json[0] != '{' && json[0] != '/' && !std::isspace(json[0]))
 	{
 		// It's not a UTF-8 BOM
 		if (size < 3 || json[0] != '\xEF' || json[1] != '\xBB' || json[2] != '\xBF')
