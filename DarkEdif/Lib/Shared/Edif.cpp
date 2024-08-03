SortaCore/MMF2Exts

commit 2450726228eb11dc092b704c015cf0d3d46afe3b

DarkEdif: Fix varying severity analysis problems
Uninitialized variables are hated by static analysis, and  they're cheap to fix.
However, static analysis did expose some areas where there were mismatches of printf variables; Unicode instead of ANSI string, for example.

DarkEdif/Lib/Shared/Edif.cpp
@@ -402,6 +402,8 @@ int Edif::Init(mv * mV, bool fusionStartupScreen)
 	Edif::ExternalJSON = (result == Edif::DependencyWasFile);
 
 	char * copy = (char *) malloc (JSON_Size + 1);
+	if (!copy)
+		return DarkEdif::MsgBox::Error(_T("Out of memory"), _T("JSON file for " PROJECT_NAME " couldn't allocate %zu bytes while loading"), JSON_Size + 1), -1;
 	memcpy (copy, JSON, JSON_Size);
 	copy [JSON_Size] = 0;
 	if ( result != Edif::DependencyWasResource )
@@ -731,6 +733,7 @@ long ActionOrCondition(void * Function, int ID, Extension * ext, const ACEInfo *
 			case Params::Compare_Time:
 			case Params::Comparison:
 				isComparisonCondition = true;
+				[[fallthrough]];
 			case Params::New_Direction:
 			case Params::Time:
 				Parameters[i] = params.GetInteger(i, info->Parameter[i].p);
@@ -1363,7 +1366,7 @@ jmethodID ExpressionManager_Android::setRetInt, ExpressionManager_Android::setRe
 struct ExpressionManager_Windows : ACEParamReader {
 	RunObject * rdPtr;
 	bool isOneOrLess = false;
-	int param;
+	int param = 0;
 	ExpressionManager_Windows(RunObject * rdPtr) : rdPtr(rdPtr)
 	{
 	}
@@ -1564,7 +1567,7 @@ ProjectFunc void PROJ_FUNC_GEN(PROJECT_NAME_RAW, _expressionJump(void * cppExtPt
 				DarkEdif::MsgBox::Error(_T("Edif::Expression() error"),
 					_T("Error calling expression \"%s\" (ID %i); parameter index %i was given a null string pointer.\n"
 					"Was the parameter type different when the expression was created in the MFA?"),
-					(const char *)CurLang["Expressions"][ID]["Title"], ID, i);
+					DarkEdif::UTF8ToTString((const char *)CurLang["Expressions"][ID]["Title"]).c_str(), ID, i);
 
 				if (ExpressionRet == ExpReturnType::String)
 					Result = (long)ext->Runtime.CopyString(_T(""));
@@ -1584,7 +1587,7 @@ ProjectFunc void PROJ_FUNC_GEN(PROJECT_NAME_RAW, _expressionJump(void * cppExtPt
 		default:
 		{
 			DarkEdif::MsgBox::Error(_T("Edif::Expression() error"), _T("Error calling expression \"%s\" (ID %i); parameter index %i has unrecognised ExpParams %hi."),
-				(const char *)CurLang["Expressions"][ID]["Title"], ID, i, (short)info->Parameter[i].ep);
+				DarkEdif::UTF8ToTString((const char *)CurLang["Expressions"][ID]["Title"]).c_str(), ID, i, (short)info->Parameter[i].ep);
 			if (ExpressionRet == ExpReturnType::String)
 				Result = (long)ext->Runtime.CopyString(_T(""));
 			goto endFunc;
@@ -1600,6 +1603,7 @@ ProjectFunc void PROJ_FUNC_GEN(PROJECT_NAME_RAW, _expressionJump(void * cppExtPt
 
 	if (Edif::SDK->ExpressionFunctions[ID] == Edif::MemberFunctionPointer(&Extension::VariableFunction))
 	{
+#pragma warning(suppress: 6001)
 		Result = ext->VariableFunction((const TCHAR *)Parameters[0], *info, Parameters);
 		_CrtCheckMemory();
 		goto endFunc;
@@ -1702,9 +1706,12 @@ int Edif::GetDependency (char *& Buffer, size_t &Size, const TCHAR * FileExtensi
 		fseek(File, 0, SEEK_SET);
 
 		Buffer = (char *) malloc(Size + 1);
+		if (!Buffer)
+			throw std::runtime_error("Out of memory");
 		Buffer[Size] = 0;
 
-		fread(Buffer, 1, Size, File);
+		if (fread(Buffer, 1, Size, File) != Size)
+			throw std::runtime_error("Reading opened file resource into RAM failed");
 
 		fclose(File);
 
@@ -1720,7 +1727,13 @@ int Edif::GetDependency (char *& Buffer, size_t &Size, const TCHAR * FileExtensi
 		return DependencyNotFound;
 
 	Size = SizeofResource (hInstLib, res);
-	Buffer = (char *) LockResource (LoadResource (hInstLib, res));
+	HGLOBAL resHandle = LoadResource(hInstLib, res);
+	if (!resHandle)
+		throw std::runtime_error("Could not load resource");
+
+	Buffer = (char *) LockResource (res);
+	if (!Buffer)
+		throw std::runtime_error("Could not lock resource");
 
 	return DependencyWasResource;
 #elif defined(__ANDROID__)
@@ -1827,13 +1840,11 @@ static void GetSiblingPath (TCHAR * Buffer, const TCHAR * FileExtension)
 
 void Edif::GetSiblingPath (TCHAR * Buffer, const TCHAR * FileExtension)
 {
-	TCHAR * Extension = (TCHAR *)
-		alloca ((_tcslen (FileExtension) + _tcslen (LanguageCode) + 2) * sizeof(TCHAR));
+	*Buffer = 0;
 
-	_tcscpy (Extension, LanguageCode);
-	_tcscat (Extension, _T ("."));
-	_tcscat (Extension, FileExtension);
-
+	TCHAR Extension[32];
+	if (_stprintf_s(Extension, std::size(Extension), _T("%s.%s"), LanguageCode, FileExtension) <= 0)
+		return DarkEdif::MsgBox::Error(_T("GetSiblingPath fail"), _T("Invalid file extension %s"), FileExtension);
 	::GetSiblingPath (Buffer, Extension);
 
 	if (*Buffer)
@@ -2022,6 +2033,10 @@ void Edif::recursive_mutex::unlock(edif_lock_debugParams)
 {
 	this->log << "Unlocked in function " << func << ", line " << line << ".\n";
 	try {
+#ifndef __analysis_assume_lock_acquired
+#define __analysis_assume_lock_acquired(x) /* no op */
+#endif
+		__analysis_assume_lock_acquired(this->intern);
 		this->intern.unlock();
 	}
 	catch (std::system_error err)

DarkEdif/Inc/Shared/DarkEdif.hpp
@@ -619,6 +619,10 @@ CONSTEXPR14_TN static_string type_name()
 // static_string to std::string_view
 #define typestr(type) std::string(type_name<type>().begin(), type_name<type>().end())
 
+// Ignore analysis warning C6287 about left and right sub-expressions being identical - they're not
+#pragma warning(push)
+#pragma warning(disable: 6287)
+
 template<int acParamIndexPlusOne, class Ret, class Struct, class... Args>
 typename std::enable_if<(acParamIndexPlusOne > 0), void>::type
 forLoopAC(unsigned int ID, const _json_value &json, std::stringstream &str, Ret(Struct::*Function)(Args...) const, Params * const condRetType = nullptr) {
@@ -713,7 +717,6 @@ forLoopAC(unsigned int ID, const _json_value &json, std::stringstream &str, Ret(
 				if (std::is_same<cppType, int>())
 					break;
 				expCppType = "int"sv;
-
 			}
 		}
 		else // unimplemented param type test
@@ -1050,6 +1053,9 @@ void LinkExpressionDebug(unsigned int ID, Ret(Struct::*Function)(Args...) const)
 
 	(Edif::SDK)->ExpressionFunctions[ID] = Edif::MemberFunctionPointer(Function);
 }
+
+// Restore analysis warning C6287
+#pragma warning(pop)
 
 // Combine the two:
 // returnType X() const;

DarkEdif/Inc/Shared/Edif.hpp
@@ -129,9 +129,9 @@ namespace Edif
 		std::vector<ACEInfo *>	ConditionInfos;
 		std::vector<ACEInfo *>	ExpressionInfos;
 
-		void ** ActionJumps;
-		void ** ConditionJumps;
-		void ** ExpressionJumps;
+		void ** ActionJumps = NULL;
+		void ** ConditionJumps = NULL;
+		void ** ExpressionJumps = NULL;
 
 		std::vector<void *> ActionFunctions;
 		std::vector<void *> ConditionFunctions;
@@ -139,11 +139,11 @@ namespace Edif
 
 		mv* mV;
 		// A fnv1a hash of all changeable property names and types, all separated by pipe. Used for property upgrades.
-		std::uint32_t jsonPropsNameAndTypesHash;
+		std::uint32_t jsonPropsNameAndTypesHash = 0;
 		// A fnv1a hash of all changeable property types, separated by pipe. Used for property upgrades.
-		std::uint32_t jsonPropsTypesHash;
+		std::uint32_t jsonPropsTypesHash = 0;
 #if EditorBuild
-		cSurface * Icon;
+		cSurface * Icon = nullptr;
 		std::unique_ptr<PropData[]> EdittimeProperties;
 #endif
 	};
@@ -169,11 +169,11 @@ namespace Edif
 #endif
 
 	public:
-		long param1, param2;
+		long param1 = 0, param2 = 0;
 
 #ifdef _WIN32
 		Runtime(Extension * ext);
-		EventParam* ParamZero;
+		EventParam* ParamZero = NULL;
 #elif defined(__ANDROID__)
 		Runtime(Extension* ext, jobject javaExtPtr);
 		global<jobject> curCEvent, curRH4ActStart;

DarkEdif/Inc/Shared/ObjectSelection.hpp
@@ -29,7 +29,7 @@ namespace DarkEdif {
 	{
 	public:
 
-		Extension * pExtension;
+		Extension * pExtension = NULL;
 
 		ObjectSelection(RunHeader * rhPtr);
 

DarkEdif/Inc/Windows/Props.hpp
@@ -319,7 +319,7 @@ class Prop_Buff : public Prop
 protected:
 	virtual ~Prop_Buff() {}
 public:
-	Prop_Buff(size_t Size_ = 0, void * Address_ = NULL) : Size(Size_)
+	Prop_Buff(std::size_t Size_ = 0, void * Address_ = nullptr) : Size(Size_)
 	{
 		if (Size == 0)
 			return; // Nothing to do
@@ -328,14 +328,12 @@ public:
 		if (Address != NULL && Address_ != NULL) // If existing address, duplicate memory
 			memcpy_s(Address, Size, Address_, Size);
 	}
-	Prop_Buff(char * Str)
-	{
-		Prop_Buff(Str ? (strlen(Str)+1)*sizeof(char) : 0, Str);
-	}
-	Prop_Buff(wchar_t * Str)
-	{
-		Prop_Buff(Str ? (wcslen(Str)+1)*sizeof(wchar_t) : 0, Str);
-	}
+	Prop_Buff(char * Str) :
+		Prop_Buff(Str ? (strlen(Str)+1)*sizeof(char) : 0, Str)
+	{ }
+	Prop_Buff(wchar_t * Str) :
+		Prop_Buff(Str ? (wcslen(Str)+1)*sizeof(wchar_t) : 0, Str)
+	{ }
 
 	virtual void Delete()
 	{
@@ -368,8 +366,8 @@ public:
 	}
 
 	// Data
-	size_t Size;
-	void * Address;
+	std::size_t Size = 0;
+	void * Address = nullptr;
 };
 
 
@@ -380,9 +378,8 @@ class Prop_AStr : public Prop
 protected:
 	virtual ~Prop_AStr() {}
 public:
-	Prop_AStr(const char * Str = NULL)
-	{
-		String = _strdup(Str ? Str : ""); // Allocate size specified
+	Prop_AStr(const char * Str = nullptr) {
+		String = _strdup(Str ? Str : "");
 	}
 #ifndef _WIN32
 	Prop_AStr(const wchar_t * Str)
@@ -391,7 +388,7 @@ public:
 			String = _strdup("");
 		else
 		{
-			size_t length = wcslen(Str);
+			std::size_t length = wcslen(Str);
 			String = (char *)calloc(length+1, sizeof(char));
 
 			if (!String)
@@ -400,21 +397,13 @@ public:
 			// TODO : change that if we use something else than CP_ACP (ACSII codepage);
 			// Use mvGetAppCodePage?
 
-			#ifdef _WIN32
 			WideCharToMultiByte(CP_ACP, 0, Str, length, String, length, NULL, NULL);
-			#elif defined(__ANDROID__)
-			// iconv is not explicitly available. Android *should* support it, but not within the default libraries.
-			__android_log_write(ANDROID_LOG_FATAL, "MMFRuntime", "Attempted to convert wchar_t * to char *, but not implemented.");
-			#else
-			for (size_t i = 0; i < length; i++)
-				String[i] = (char)(Str[i] & 0x00FF);
-			#endif
 
 			String[length] = 0;	// Force null ending
 		}
 	}
 #endif
-	Prop_AStr(size_t size)
+	Prop_AStr(std::size_t size)
 	{
 		if (size > 0)
 			String = (char *)calloc(size, sizeof(char)); // Allocate size specified
@@ -456,8 +445,8 @@ public:
 	}
 
 	// Data
-	unsigned int pad;	// This is a required waste of space or MMF will die trying to read it.
-	char * String;
+	unsigned int pad = 0;	// This is a required waste of space or MMF will die trying to read it.
+	char * String = nullptr;
 };
 
 // String (UNICODE)
@@ -497,12 +486,9 @@ public:
 #endif
 		}
 	}
-	Prop_WStr(size_t Size)
+	Prop_WStr(std::size_t Size)
 	{
-		if (Size > 0)
-			String = (wchar_t *)calloc(Size, sizeof(wchar_t)); // Allocate size specified
-		else
-			String = _wcsdup(L"");
+		String = (wchar_t *)calloc(Size > 0 ? Size : 1, sizeof(wchar_t)); // Allocate size specified
 	}
 
 	virtual void Delete()
@@ -540,7 +526,7 @@ public:
 
 	// Data
 	unsigned int pad = 0;	// This is a required waste of space or MMF will die trying to read it.
-	wchar_t * String;
+	wchar_t * String = nullptr;
 };
 
 

DarkEdif/Inc/Windows/WindowsDefines.hpp
@@ -84,7 +84,11 @@ using UShortTCHAR = unsigned short;
 
 using WindowHandleType = HWND;
 
-// We hide error 4200, caused by zero-length arrays causing constructors to not be created.
+// We hide compiler warning 4200, caused by zero-length arrays causing perhaps
+// unexpected default ctor behaviour. However, despite being a warning that claims
+// these are non-standard, it is a valid ISO C99 standard; called a "flexible array member",
+// for docs see C11, 6.7.21/18.
 // struct { short some_var; int end_array[]; } allows struct to be malloc'd with end_array
+// a runtime-determined size, rather than compile-time. So some_var is a header of sorts.
 // It IS non-standard to use [0] instead of [] for these members, though GCC supports it.
 #pragma warning (disable: 4200)

commit 0bfcf42772e413208b5779cfd14a625de55d96ef

DarkEdif: Fix for recent JSON loading problem
Introduced in commit 2450726228eb11dc092b704c015cf0d3d46afe3b.

DarkEdif/Lib/Shared/Edif.cpp
@@ -1731,7 +1731,7 @@ int Edif::GetDependency (char *& Buffer, size_t &Size, const TCHAR * FileExtensi
 	if (!resHandle)
 		throw std::runtime_error("Could not load resource");
 
-	Buffer = (char *) LockResource (res);
+	Buffer = (char *) LockResource (resHandle);
 	if (!Buffer)
 		throw std::runtime_error("Could not lock resource");
 